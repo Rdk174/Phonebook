bb0f6deea8729a1f222da8e474f847a9b0e96da9
Rdk174 <noname_rf@mail.ru>
2018-05-25 17:30:52 +0500

    Add classes, some logic. TODO: serialize, deserialize JSON

--- Abonent.cs
+++ Abonent.cs
@@ -8,5 +8,38 @@ namespace Phonebook
 {
     class Abonent
     {
+        private string name;
+        private string phoneNumber;
+
+        public string Name
+        {
+            set
+            {
+                if(!string.IsNullOrEmpty(value))
+                    name = value.Trim();
+            }
+            get
+            {
+                return name;
+            }
+        }
+        public string PhoneNumber
+        {
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    phoneNumber = value.Trim();
+            }
+            get
+            {
+                return phoneNumber;
+            }
+        }
+        public Abonent(string name, string phoneNumber)
+        {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+        }
+
     }
 }
